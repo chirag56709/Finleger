0bbb3b7d7844e34cc920d5189d8168627eda652f
Chirag Joshi <140572992+chirag56709@users.noreply.github.com>
2025-12-24 13:04:35 +0530

    fix vercel serverless crash

---
+++ server/index.js
@@ -0,0 +1,6 @@
+const app = require("./server");
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- server/server.js
+++ server/server.js
@@ -1,6 +1,24 @@
-const app = require("./app");
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./db");
+const authRoutes = require("./routes/auth");
 
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log("Server running on port 5000");
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.use("/api/auth", authRoutes);
+
+// test route
+app.get("/", (req, res) => {
+  res.send("FinLedger API running");
 });
+
+// connect db (important)
+connectDB();
+
+// ❌ app.listen() HATA DO
+module.exports = app;
